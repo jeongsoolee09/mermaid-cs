b0cb94f97411050c7e0c7c9b110d237453a459ff
Jeongsoo Lee <jslee@unknown5eaf974a8380.attlocal.net>
2022-11-15 10:39:37 -0800

    initial

---
+++ src/SequenceDiagram.cs
@@ -0,0 +1,13 @@
+
+// SequenceDiagram sd = sequenceDiagram()
+// 	        	.add(loop("until dead"))
+// 			.add(solidArrow("alice", "bob", "hihi"))
+// 			.add(solidArrow("bob", "alice", "hoho"))
+// 			.add(optional("hoho")
+// 				.add(solidArrow("alice", "bob", "hihi"))
+// 				.add(alternative("x = 1", solidArrow("alice", "bob", "hihi"))  // alternative takes variadic arguments
+// 				           .cond("x = 2", solidArrow("bob", "join", "hihi"))
+// 			   		   .cond("x = 3", solidArrow("john", "alice", "hihi"))))
+// 	                .add(parallel("alice to bob", solidArrow("alice", "bob", "hihi"))
+// 		                .cond("bob to alice", solidArrow("bob", "alice", "hihi")))
+				    
