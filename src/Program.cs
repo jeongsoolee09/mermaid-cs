b03a6cb4eb767f15366cdecaa79542ccedc124eb
이정수 <jslee@ijeongsus-iMac.attlocal.net>
2022-11-17 18:33:36 -0800

    minor formatting

--- src/Program.cs
+++ src/Program.cs
@@ -3,8 +3,8 @@ using static Sequence.Lang;
 using System;
 
 string alice = "alice";
-string bob = "bob";
-string john = "john";
+string bob   = "bob";
+string john  = "john";
 
 string sayhi = "hihi";
 string sayho = "hoho";
@@ -16,10 +16,10 @@ SequenceDiagram sd = (SequenceDiagram) sequenceDiagram()
 			     .add(optional("hoho")
 				     .add(solidArrow(alice, bob, sayhi))
 				     .add(alternative("x = 1", solidArrow(alice, bob, sayhi))
-				                .cond("x = 2", solidArrow(bob, john, sayhi))
-			   		        .cond("x = 3", solidArrow(john, alice, sayhi))))
+					        .cond("x = 2", solidArrow(bob, john, sayhi))
+					        .cond("x = 3", solidArrow(john, alice, sayhi))))
 	                     .add(parallel("alice to bob", solidArrow(alice, bob, sayhi))
-				  .cond("bob to alice", solidArrow(bob, alice, sayho))));
+				     .cond("bob to alice", solidArrow(bob, alice, sayho))));
 
 
 Console.WriteLine(sd);
